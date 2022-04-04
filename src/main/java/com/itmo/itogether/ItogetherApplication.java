337f378afa365eab1f7f71798e1136fac623507c
noparamin <noparamin@naver.com>
2022-04-04 19:59:04 +0900

    first commit

---
+++ src/main/java/com/itmo/itogether/ItogetherApplication.java
@@ -0,0 +1,13 @@
+package com.itmo.itogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItogetherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itmo/itogether/ItogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itmo.itogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
